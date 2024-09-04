543f4a65f5f45d4dbdb90636426cb4fce8d95507
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-09-03 20:27:13 -0400

    feat: first 7 tasks

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -1,4 +1,9 @@
 export const tasks = {
-  "Insert line above": ["Control", "Shift", "Enter"],
-  "Insert line below": ["Control", "Enter"],
-}
+  "Insert line above": { combos: [["Control", "Shift", "Enter"], ["Shift", "Control", "Enter"]], src: "src/lib/videos/insertLineAbove.mp4" },
+  "Copy line down": { combos: [["Alt", "Shift", "ArrowDown"], ["Shift", "Alt", "ArrowDown"]], src: "src/lib/videos/copyLineDown.mp4" },
+  "Copy line up": { combos: [["Alt", "Shift", "ArrowUp"], ["Shift", "Alt", "ArrowUp"]], src: "src/lib/videos/copyLineUp.mp4" },
+  "Delete line": { combos: [["Control", "Shift", "K"], ["Shift", "Control", "K"]], src: "src/lib/videos/deleteLine.mp4" },
+  "Insert line below": { src: "src/lib/videos/insertLineBelow.mp4", combos: [["Control", "Enter"]] },
+  "Move line down": { src: "src/lib/videos/moveLineDown.mp4", combos: [["Alt", "ArrowDown"]] },
+  "Move line up": { src: "src/lib/videos/moveLineUp.mp4", combos: [["Alt", "ArrowUp"]] },
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,44 @@
 <script lang="ts">
-  import "../app.css";
   import { tasks } from "$lib/tasks";
 
+  import "../app.css";
+
+  const getRandomIntInclusive = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+  let randomTaskIndex = $state(getRandomIntInclusive(0, Object.keys(tasks).length - 1));
   let heldKeys: string[] = $state([]);
-  let isSolved = $derived(heldKeys.toString() === Object.values(tasks)[1].toString());
+  let src = $derived(Object.values(tasks)[randomTaskIndex].src);
+  let isSolved = $derived.by(() => {
+    const combos = Object.values(tasks)[randomTaskIndex].combos;
+    for (let i = 0; i < combos.length; i++) if (heldKeys.toString() === combos[i].toString()) return true;
+    return false;
+  });
 
   const onkeydown = (e: KeyboardEvent) => {
     e.preventDefault();
     if (e.repeat) return;
     heldKeys.push(e.key);
-  }
+  };
 
-  const onkeyup = (e: KeyboardEvent) => { heldKeys = heldKeys.filter(key => key !== e.key); }
+  const onkeyup = (e: KeyboardEvent) => { heldKeys = heldKeys.filter(key => key !== e.key); };
+
+  $effect(() => {
+    if (isSolved) {
+      heldKeys = [];
+      const temp = randomTaskIndex;
+      while (temp === randomTaskIndex) randomTaskIndex = getRandomIntInclusive(0, Object.keys(tasks).length - 1);
+    }
+  });
 </script>
+
+<div class="absolute right-0 top-0 scale-50">
+  <a href="https://github.com/Tree52/VSCode-Trainer">
+    <svg xmlns="http://www.w3.org/2000/svg" height="96" width="98">
+      <!-- eslint-disable-next-line -->
+      <path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill-rule="evenodd" clip-rule="evenodd" fill="#fff"/>
+    </svg>
+  </a>
+</div>
 
 <main class="flex flex-1 flex-col items-center justify-center">
   <div class="text-white">
@@ -25,11 +51,11 @@
     {/each}
   </div>
   <div>
-    {Object.keys(tasks)[1]}
+    {Object.keys(tasks)[randomTaskIndex]}
   </div>
-  <div>
-    {isSolved}
+  <div class="w-1/2 p-2">
+    <video autoplay {src} muted loop></video>
   </div>
 </main>
 
-<svelte:window {onkeydown} {onkeyup} onblur={() => { heldKeys = []; }}></svelte:window>
+<svelte:window onblur={() => { heldKeys = []; }} {onkeydown} {onkeyup}></svelte:window>
