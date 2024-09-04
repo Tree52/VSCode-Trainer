84c5203729aa0f6deaf7788fb80b03060f57f43e
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-09-03 22:17:41 -0400

    fix: build working

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -1,9 +1,17 @@
+import insertLineAbove from "$lib/videos/insertLineAbove.mp4";
+import insertLineBelow from "$lib/videos/insertLineBelow.mp4";
+import copyLineDown from "$lib/videos/copyLineDown.mp4";
+import moveLineDown from "$lib/videos/moveLineDown.mp4";
+import copyLineUp from "$lib/videos/copyLineUp.mp4";
+import deleteLine from "$lib/videos/deleteLine.mp4";
+import moveLineUp from "$lib/videos/moveLineUp.mp4";
+
 export const tasks = {
-  "Insert line above": { combos: [["Control", "Shift", "Enter"], ["Shift", "Control", "Enter"]], src: "src/lib/videos/insertLineAbove.mp4" },
-  "Copy line down": { combos: [["Alt", "Shift", "ArrowDown"], ["Shift", "Alt", "ArrowDown"]], src: "src/lib/videos/copyLineDown.mp4" },
-  "Copy line up": { combos: [["Alt", "Shift", "ArrowUp"], ["Shift", "Alt", "ArrowUp"]], src: "src/lib/videos/copyLineUp.mp4" },
-  "Delete line": { combos: [["Control", "Shift", "K"], ["Shift", "Control", "K"]], src: "src/lib/videos/deleteLine.mp4" },
-  "Insert line below": { src: "src/lib/videos/insertLineBelow.mp4", combos: [["Control", "Enter"]] },
-  "Move line down": { src: "src/lib/videos/moveLineDown.mp4", combos: [["Alt", "ArrowDown"]] },
-  "Move line up": { src: "src/lib/videos/moveLineUp.mp4", combos: [["Alt", "ArrowUp"]] },
+  "Insert line above": { combos: [["Control", "Shift", "Enter"], ["Shift", "Control", "Enter"]], src: insertLineAbove },
+  "Copy line down": { combos: [["Alt", "Shift", "ArrowDown"], ["Shift", "Alt", "ArrowDown"]], src: copyLineDown },
+  "Copy line up": { combos: [["Alt", "Shift", "ArrowUp"], ["Shift", "Alt", "ArrowUp"]], src: copyLineUp },
+  "Delete line": { combos: [["Control", "Shift", "K"], ["Shift", "Control", "K"]], src: deleteLine },
+  "Insert line below": { combos: [["Control", "Enter"]], src: insertLineBelow },
+  "Move line down": { combos: [["Alt", "ArrowDown"]], src: moveLineDown },
+  "Move line up": { combos: [["Alt", "ArrowUp"]], src: moveLineUp },
 };
