59373a9ef26019f36c3c2ea5a4ec06be41dbd8c5
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-09-13 05:05:27 -0400

    refactor: move videos to static folder

--- src/lib/refs.svelte.ts
+++ src/lib/refs.svelte.ts
@@ -1,18 +1,3 @@
-import addLineComment from "$lib/videos/addLineComment.mp4";
-import copyLineDown from "$lib/videos/copyLineDown.mp4";
-import copyLineUp from "$lib/videos/copyLineUp.mp4";
-import deleteLine from "$lib/videos/deleteLine.mp4";
-import fold from "$lib/videos/fold.mp4";
-import goToBracket from "$lib/videos/goToBracket.mp4";
-import indentLine from "$lib/videos/indentLine.mp4";
-import insertLineAbove from "$lib/videos/insertLineAbove.mp4";
-import insertLineBelow from "$lib/videos/insertLineBelow.mp4";
-import moveLineDown from "$lib/videos/moveLineDown.mp4";
-import moveLineUp from "$lib/videos/moveLineUp.mp4";
-import outdentLine from "$lib/videos/outdentLine.mp4";
-import toggleWordWrap from "$lib/videos/toggleWordWrap.mp4";
-import unfold from "$lib/videos/unfold.mp4";
-
 type Task = {
   combos: string[];
   commands: string[];
@@ -38,20 +23,20 @@ const ref = <T>(initial: T) => {
 // Preferences: Open Default Keyboard Shortcuts (JSON)
 // https://github.com/codebling/vs-code-default-keybindings
 export const tasks = ref<Tasks>({
-  "Add Line Comment / Toggle Comment": { combos: ["ctrl+k ctrl+c", "cmd+k cmd+c", "ctrl+/", "cmd+/"], commands: ["editor.action.addCommentLine", "editor.action.commentLine"], src: addLineComment },
-  "Copy Line Down": { combos: ["shift+alt+down", "ctrl+shift+alt+down"], commands: ["editor.action.copyLinesDownAction"], src: copyLineDown },
-  "Copy Line Up": { combos: ["shift+alt+up", "ctrl+shift+alt+up"], commands: ["editor.action.copyLinesUpAction"], src: copyLineUp },
-  "Delete Line": { combos: ["ctrl+shift+k", "shift+cmd+k"], commands: ["editor.action.deleteLines"], src: deleteLine },
-  "Fold": { combos: ["ctrl+shift+[", "alt+cmd+["], commands: ["editor.fold"], src: fold },
-  "Go to Bracket": { combos: ["ctrl+shift+\\", "shift+cmd+\\"], commands: ["editor.action.jumpToBracket"], src: goToBracket },
-  "Indent Line": { combos: ["ctrl+]", "cmd+]"], commands: ["editor.action.indentLines"], src: indentLine },
-  "Insert Line Above": { combos: ["ctrl+shift+enter", "shift+cmd+enter"], commands: ["editor.action.insertLineBefore"], src: insertLineAbove },
-  "Insert Line Below": { combos: ["ctrl+enter", "cmd+enter"], commands: ["editor.action.insertLineAfter"], src: insertLineBelow },
-  "Move Line Down": { combos: ["alt+down"], commands: ["editor.action.moveLinesDownAction"], src: moveLineDown },
-  "Move Line Up": { combos: ["alt+up"], commands: ["editor.action.moveLinesUpAction"], src: moveLineUp },
-  "Outdent Line / outdent": { combos: ["ctrl+[", "shift+tab", "cmd+["], commands: ["editor.action.outdentLines", "outdent"], src: outdentLine },
-  "Unfold": { combos: ["ctrl+shift+]", "alt+cmd+]"], commands: ["editor.unfold"], src: unfold },
-  "View: Toggle Word Wrap": { combos: ["alt+z"], commands: ["editor.action.toggleWordWrap"], src: toggleWordWrap },
+  "Add Line Comment / Toggle Comment": { combos: ["ctrl+k ctrl+c", "cmd+k cmd+c", "ctrl+/", "cmd+/"], commands: ["editor.action.addCommentLine", "editor.action.commentLine"], src: "videos/basicEditing/addLineComment.mp4" },
+  "Copy Line Down": { combos: ["shift+alt+down", "ctrl+shift+alt+down"], commands: ["editor.action.copyLinesDownAction"], src: "videos/basicEditing/copyLinesDownAction.mp4" },
+  "Copy Line Up": { combos: ["shift+alt+up", "ctrl+shift+alt+up"], commands: ["editor.action.copyLinesUpAction"], src: "videos/basicEditing/copyLinesUpAction.mp4" },
+  "Delete Line": { combos: ["ctrl+shift+k", "shift+cmd+k"], commands: ["editor.action.deleteLines"], src: "videos/basicEditing/deleteLines.mp4" },
+  "Fold": { combos: ["ctrl+shift+[", "alt+cmd+["], commands: ["editor.fold"], src: "videos/basicEditing/fold.mp4" },
+  "Go to Bracket": { combos: ["ctrl+shift+\\", "shift+cmd+\\"], commands: ["editor.action.jumpToBracket"], src: "videos/basicEditing/jumpToBracket.mp4" },
+  "Indent Line": { combos: ["ctrl+]", "cmd+]"], commands: ["editor.action.indentLines"], src: "videos/basicEditing/indentLines.mp4" },
+  "Insert Line Above": { combos: ["ctrl+shift+enter", "shift+cmd+enter"], commands: ["editor.action.insertLineBefore"], src: "videos/basicEditing/insertLineBefore.mp4" },
+  "Insert Line Below": { combos: ["ctrl+enter", "cmd+enter"], commands: ["editor.action.insertLineAfter"], src: "videos/basicEditing/insertLineAfter.mp4" },
+  "Move Line Down": { combos: ["alt+down"], commands: ["editor.action.moveLinesDownAction"], src: "videos/basicEditing/moveLinesDownAction.mp4" },
+  "Move Line Up": { combos: ["alt+up"], commands: ["editor.action.moveLinesUpAction"], src: "videos/basicEditing/moveLinesUpAction.mp4" },
+  "Outdent Line / outdent": { combos: ["ctrl+[", "shift+tab", "cmd+["], commands: ["editor.action.outdentLines", "outdent"], src: "videos/basicEditing/outdentLines.mp4" },
+  "Unfold": { combos: ["ctrl+shift+]", "alt+cmd+]"], commands: ["editor.unfold"], src: "videos/basicEditing/unfold.mp4" },
+  "View: Toggle Word Wrap": { combos: ["alt+z"], commands: ["editor.action.toggleWordWrap"], src: "videos/basicEditing/toggleWordWrap.mp4" },
 });
 
 export const enterPressed = ref(false);
